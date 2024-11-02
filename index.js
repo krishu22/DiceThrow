2d11130906b692215f75efa591d3c1386322b57e
krishu22 <krishuugrg@gmail.com>
2024-11-02 10:14:06 +0530

    first push

---
+++ index.js
@@ -0,0 +1,67 @@
+let randomNumber1 = Math.floor(1+Math.random()*6);
+
+if(randomNumber1===1){
+    document.getElementsByClassName("img1")[0].src = "images/dice1.png";
+}
+
+if(randomNumber1===2){
+    document.getElementsByClassName("img1")[0].src = "images/dice2.png";
+}
+
+if(randomNumber1===3){
+    document.getElementsByClassName("img1")[0].src = "images/dice3.png";
+}
+
+if(randomNumber1===4){
+    document.getElementsByClassName("img1")[0].src = "images/dice4.png";
+}
+
+if(randomNumber1===5){
+    document.getElementsByClassName("img1")[0].src = "images/dice5.png";
+}
+
+if(randomNumber1===6){
+    document.getElementsByClassName("img1")[0].src = "images/dice6.png";
+}
+
+
+
+let randomNumber2 = Math.floor(1+Math.random()*6);
+
+if(randomNumber2===1){
+    document.getElementsByClassName("img2")[0].src = "images/dice1.png";
+}
+
+if(randomNumber2===2){
+    document.getElementsByClassName("img2")[0].src = "images/dice2.png";
+}
+
+if(randomNumber2===3){
+    document.getElementsByClassName("img2")[0].src = "images/dice3.png";
+}
+
+if(randomNumber2===4){
+    document.getElementsByClassName("img2")[0].src = "images/dice4.png";
+}
+
+if(randomNumber2===5){
+    document.getElementsByClassName("img2")[0].src = "images/dice5.png";
+}
+
+if(randomNumber2===6){
+    document.getElementsByClassName("img2")[0].src = "images/dice6.png";
+}
+
+
+
+if(randomNumber1>randomNumber2){
+    document.getElementsByTagName("h1")[0].textContent="Player 1 wins!";
+}
+
+else if(randomNumber1<randomNumber2){
+    document.getElementsByTagName("h1")[0].textContent="Player 2 wins!";
+}
+
+else if(randomNumber1==randomNumber2){
+    document.getElementsByTagName("h1")[0].textContent="draw!";
+}
